932697f21e33e01675cc0d2a7ad237a0a6149167
sara <sara.romano.15@gmail.com>
2020-04-23 18:56:52 +0200

    Adding fake node imu + improvement to Smart_node

--- src/Smartwatch_node.py
+++ src/Smartwatch_node.py
@@ -2,40 +2,51 @@
 import rospy
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Header
+#from multimodal_dataset_creation.msg import FakeImu1 #import fake_msg
 
 
-def callback1(data):
-    data.data
-
+#defining node
 class Fromsmart(object):
     def init(self):
+        #data from GUI_node
+        self.data1 = Header()
+        self.flagstart =False
+        #frequency
         self.update_rate = 100
+        #data frum Smartwatch
         self.data = Imu()
         self.dataPublished = False
 
         self.pub_imu = rospy.Publisher('/imu_data', Imu, queue_size= 1 )
-        rospy.Subscriber('/smartwatch_status', Header, callback1)
-        rospy.Subscriber('/inertial', Imu, self.callbackImu)
+        rospy.Subscriber('/smartwatch_status', Header, self.callback1)
 
+        #rospy.Subscriber('/inertial', Imu, self.callbackImu)
+        rospy.Subscriber('/fake_data_Imu', Imu, self.callbackImu)
+
+    def callback1(self,data):
+        self.data1 = data
+        self.flagstart = True
 
 
     def callbackImu(self,data):
-            self.data = data
-            self.dataPublished = True
+        self.data = data
+
+        self.dataPublished = True #active bool
 
     def run(self):
         self.init()
         r = rospy.Rate(self.update_rate)
         while True:
             try:
-                if self.dataPublished:
-                    self.pub_imu.publish(self.data)
-                    self.dataPublished =False
+                #if self.dataPublished and self.flagstart:
+
+                    self.pub_imu.publish(self.data) #publish data from /fake_data_imu to topic /imu_data
+                    self.dataPublished =False #disable bool
             except KeyboardInterrupt:
                 break
 
 def main():
-    rospy.init_node('smartdata', disable_signals=True)
+    rospy.init_node('smartdata', disable_signals=True) #create node
     smartdata = Fromsmart()
     smartdata.run()
 
