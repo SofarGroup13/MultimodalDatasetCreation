06429e4e81a2fba81ee36eeb28e53b243bd05eac
Silvana Andrea <silvanaandrea.civiletto@gmail.com>
2020-04-28 17:46:44 +0200

    Added comments

--- src/lib/Tools/bag2csv-conversion.py
+++ src/lib/Tools/bag2csv-conversion.py
@@ -3,7 +3,7 @@ import rosbag, os, rospkg, csv, sys
 from std_msgs.msg import Header, String
 from sensor_msgs.msg import PointCloud2, Imu, Point
 
-if (len(sys.argv) > 2):
+if (len(sys.argv) > 2): ##Verify the correctness of input arguments
     print(sys.argv)
     print("invalid number of argument")
     sys.exit(1)
@@ -12,14 +12,14 @@ else:
 
 filesName = ['pointclod2', 'gestures', 'point', 'imu']
 
-for fileName in filesName:
+for fileName in filesName: ##Access to rosbag
     bag = rosbag.Bag(bagFolder + "/" + fileName + ".bag")
-    csvName = bagFolder + "/" + fileName + ".csv"
+    csvName = bagFolder + "/" + fileName + ".csv" ##Creation of new csv file for each topic
     if not(os.path.isfile(csvName)):
         with open(csvName, 'w+') as csvfile:
             filewriter = csv.writer(csvfile, delimiter = ',')
             firstIteration = True
-        for topic, msg, t in bag.read_messages():
+        for topic, msg, t in bag.read_messages(): ##Recovery of topic list from rosbag
                     values = []
 if fileName == 'pointcloud2':   
                         if firstIteration:
